08894b18a2fbeb6c8348655fcd916e7d042d194e
chiajun93 <chiajun@iastate.edu>
2015-10-06 01:19:06 -0500

    Added buttons on calculator

--- calculator.js
+++ calculator.js
@@ -4,38 +4,75 @@
 //
 
 // 
+
 var Calc = {
 
-Model : {
-},
+  Model : {
+    preVal : 0,
+    total: 0,
+    op: "",
+    display : false
+  },
 
 
-View : {
-  textRow : {id: "textRow", type: "text", value: "", onclick:""},
-  button7 : {id: "button7", type: "button", value: 7, onclick:""},
-  button8 : {id: "button8", type: "button", value: 8, onclick:""},
-  button9 : {id: "button9", type: "button", value: 9, onclick:""},
-  button4 : {id: "button4", type: "button", value: 4, onclick:""},
-  button5 : {id: "button4", type: "button", value: 5, onclick:""},
-  button6 : {id: "button4", type: "button", value: 6, onclick:""},
-  button1 : {id: "button4", type: "button", value: 1, onclick:""},
-  button2 : {id: "button4", type: "button", value: 2, onclick:""},
-  button3 : {id: "button4", type: "button", value: 3, onclick:""},
-  button0 : {id: "button4", type: "button", value: 0, onclick:""},
-  buttonDot : {id: "button4", type: "button", value: ".", onclick:""},
-  buttonMinus : {id: "button4", type: "button", value: "-", onclick:""},
-  buttonTime : {id: "button4", type: "button", value: "*", onclick:""},
-  buttonDiv : {id: "button4", type: "button", value: "/", onclick:""},
-  buttonPlus : {id: "buttonPlus", type: "button", value: "+", onclick:""}
-},
+  View : {
+    textRow : {id: "textRow", type: "text", value: "", onclick:""},
+    button7 : {id: "button7", type: "button", value: 7, onclick:""},
+    button8 : {id: "button8", type: "button", value: 8, onclick:""},
+    button9 : {id: "button9", type: "button", value: 9, onclick:""},
+    button4 : {id: "button4", type: "button", value: 4, onclick:""},
+    button5 : {id: "button5", type: "button", value: 5, onclick:""},
+    button6 : {id: "button6", type: "button", value: 6, onclick:""},
+    button1 : {id: "button1", type: "button", value: 1, onclick:""},
+    button2 : {id: "button2", type: "button", value: 2, onclick:""},
+    button3 : {id: "button3", type: "button", value: 3, onclick:""},
+    button0 : {id: "button0", type: "button", value: 0, onclick:""},
+    buttonDot : {id: "buttonDot", type: "button", value: ".", onclick:""},
+    buttonMinus : {id: "buttonMinus", type: "button", value: "-", onclick:""},
+    buttonTimes : {id: "buttonTimes", type: "button", value: "*", onclick:""},
+    buttonDiv : {id: "buttonDiv", type: "button", value: "/", onclick:""},
+    buttonEqual : {id: "buttonEqual", type: "button", value: "=", onclick:""},
+    buttonClear : {id: "buttonClear", type: "button", value: "C", onclick:""},
+    buttonMClear : {id: "buttonMClear", type: "button", value: "MC", onclick:""},
+    buttonMMinus : {id: "buttonMMinus", type: "button", value: "M-", onclick:""},
+    buttonMPlus : {id: "buttonMPlus", type: "button", value: "M+", onclick:""},
+    buttonPlus : {id: "buttonPlus", type: "button", value: "+", onclick:""}
+  },
 
-Controller : {
+  Controller : {
+    buttonsHandler : function(currVal) {
 
-},
+     
+        textRow.value += currVal;
+      
 
-run : function() {
-  Calc.attachHandlers();
-  console.log(Calc.display());
+    },
+
+    opsHandler: function(op){
+      if(op === "+"){
+        Calc.Model.total += preVal;
+      }
+
+      else if(op === "-"){
+        Calc.Controller.op = "-";
+
+      }
+
+      else if(op === "*"){
+        Calc.Controller.op = "*";
+
+      }
+
+      else if(op === "/"){
+        Calc.Controller.op = "/";
+
+      }
+    }
+  },
+
+  run : function() {
+    Calc.attachHandlers();
+  // console.log(Calc.display());
   return Calc.display();
 },
 
@@ -53,25 +90,49 @@ displayElement : function (element) {
 
 display : function() {
   var s;
-  s = "<table id=\"myTable\" border=2>"
+  s = "<table id=\"myTable\" border=4>"
   s += "<tr><td>" + Calc.displayElement(Calc.View.textRow) + "</td></tr>";
   s += "<tr><td>";
   s += Calc.displayElement(Calc.View.button7) + "\t";
   s += Calc.displayElement(Calc.View.button8) + "\t";
   s += Calc.displayElement(Calc.View.button9) + "\t";
   s += Calc.displayElement(Calc.View.buttonPlus);
-  s += "<tr><td>";
+  s += "<br>";
   s += Calc.displayElement(Calc.View.button4) + "\t";
+  s += Calc.displayElement(Calc.View.button5) + "\t";
+  s += Calc.displayElement(Calc.View.button6) + "\t";
+  s += Calc.displayElement(Calc.View.buttonMinus) + "\t";
+  s += "<br>";
+  s += Calc.displayElement(Calc.View.button1) + "\t";
+  s += Calc.displayElement(Calc.View.button2) + "\t";
+  s += Calc.displayElement(Calc.View.button3) + "\t";
+  s += Calc.displayElement(Calc.View.buttonTimes) + "\t";
+  s += "<br>";
+  s += Calc.displayElement(Calc.View.button0) + "\t";
+  s += Calc.displayElement(Calc.View.buttonDot) + "\t";
+  s += Calc.displayElement(Calc.View.buttonEqual) + "\t";
+  s += Calc.displayElement(Calc.View.buttonDiv) + "\t";
+  s += "<br>";
+  s += Calc.displayElement(Calc.View.buttonClear) + "\t";
+  s += Calc.displayElement(Calc.View.buttonMClear) + "\t";
+  s += Calc.displayElement(Calc.View.buttonMMinus) + "\t";
+  s += Calc.displayElement(Calc.View.buttonMPlus) + "\t";
+  
   s += "</tr></td></table>";
   return s;
 },
 
 attachHandlers : function() {
-  Calc.View.button7.onclick = "Calc.button7Handler()"; 
+  for (var i = 0; i <= 9; i++) {
+   Calc.View["button" + i].onclick ="Calc.Controller.buttonsHandler(this.value)";
+ }
+
+ Calc.View.buttonPlus.onclick = "Calc.Controller.buttonsHandler(this.value)";
+ Calc.View.buttonMinus.onclick = "Calc.Controller.buttonsHandler(this.value)";
+ Calc.View.buttonTimes.onclick = "Calc.Controller.buttonsHandler(this.value)";
+ Calc.View.buttonDiv.onclick = "Calc.Controller.buttonsHandler(this.value)";  
 },
 
-button7Handler : function() {
-  alert("Hi");
-}
+
 
 } // end of Calc;
